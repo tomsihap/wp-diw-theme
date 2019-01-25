fc6667c3da50d5dc55a25f9a3263f033cabce212
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2019-01-25 14:23:52 +0100

    ajout de la sidebar à widgets

--- functions.php
+++ functions.php
@@ -5,5 +5,18 @@ function wf3_enqueue_scripts() {
     wp_enqueue_style('master', get_template_directory_uri() . '/style.css');
 }
 
-
 add_action('wp_enqueue_scripts', 'wf3_enqueue_scripts');
+
+function wf3_widgets_init() {
+    register_sidebar([
+        'name'          => "Sidebar",
+        'id'            => 'sidebar-1',
+        'description'   => "Endroit où on mettra les widgets",
+        'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</li>',
+        'before_title'  => '<h2 class="widgettitle">',
+        'after_title'   => '</h2>',
+    ]);
+}
+
+add_action( 'widgets_init', 'wf3_widgets_init' );
--- index.php
+++ index.php
@@ -3,23 +3,43 @@
 
     <section id="content mt-5">
 
-    <?php   if ( have_posts() ) : ?>
-    <?php       while ( have_posts() ) : the_post(); ?>
+    <div class="row">
+        <!-- Posts -->
+        <div class="col-8">
+            <?php   if ( have_posts() ) : ?>
+            <?php       while ( have_posts() ) : the_post(); ?>
 
-                <div class="card mt-3">
-                    <h3>
+                        <div class="card mt-3">
+                            <h3>
 
-                        <?php the_shortlink( get_the_title() ); ?>
+                                <?php the_shortlink( get_the_title() ); ?>
 
-                        (<?php the_category(   get_the_title()   ); ?>)
-                    </h3>
-                    <small><?php the_author(); ?> - Rédigé le <?php the_date(); ?> à <?php the_time(); ?></small>
-                    <p><?php the_excerpt(); ?></p>
+                                (<?php the_category(   get_the_title()   ); ?>)
+                            </h3>
+                            <small><?php the_author(); ?> - Rédigé le <?php the_date(); ?> à <?php the_time(); ?></small>
+                            <p><?php the_excerpt(); ?></p>
+
+                        </div>
+
+            <?php      endwhile; ?>
+            <?php endif; ?>
+        </div>
+
+        <!-- Sidebar -->
+        <div class="col-4">
+            <?php if (is_active_sidebar('sidebar-1')): ?>
+
+                <div class="card">
+                
+                    <?php dynamic_sidebar('sidebar-1'); ?>
 
                 </div>
 
-    <?php      endwhile; ?>
-    <?php endif; ?>
+            <?php endif;?>
+        </div>
+    </div>
+
+   
 
     </section>
 
